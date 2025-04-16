81a21a645face54b7c2a39ef10c1e67b255518b9
Jonathan-9717 <201195385+Jonathan-9717@users.noreply.github.com>
2025-04-16 14:15:45 -0700

    Complete javascript-exercise 07_tempConversion
    Create two functions. One function to convert
    Fahrenheit to Celsius. Another function to convert
    Celsius to Celsius. Rounding the result to one
    decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let fahrenheitCoverted = (x - 32) * (5 / 9);
+  let fixedResult = +fahrenheitCoverted.toFixed(1);
+
+  if (fixedResult === 0.0) {
+    return 0;
+  } else {
+    return fixedResult;
+  };
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  let celsiusConverted = (y * (9 / 5)) + 32;
+  let fixedResult = +celsiusConverted.toFixed(1);
+
+  if (fixedResult === 0.0) {
+    return 0;
+  } else {
+    return fixedResult;
+  };
+  
 };
 
 // Do not edit below this line
